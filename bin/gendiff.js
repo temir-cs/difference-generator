3aa7496cc978322895e7b00bb6c9814228b70a67
temirlan.akhanov@yandex.ru <temir-cs@DESKTOP-7KH1KTM.localdomain>
2020-08-22 18:28:53 +0600

    created genDiff logic for two flat .json files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,15 +1,23 @@
 #! /usr/bin/env node
 import program from 'commander';
 import packageConfig from '../package.json';
+import genDiff from '../src/index.js';
+
+let path1;
+let path2;
 
 program.version(packageConfig.version)
   .description('Compares two configuration files and shows a difference.')
   .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {
+    path1 = filepath1;
+    path2 = filepath2;
+  })
   .option('-f, --format [type]', 'output format');
 
-/*
-program.version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
-*/
 program.parse(process.argv);
+
 // console.log('Welcome to GenDiff App!');
+
+const output = genDiff(path1, path2);
+console.log(output);
--- src/index.js
+++ src/index.js
@@ -1,5 +1,56 @@
-/* const { program } = require('commander');
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import _ from 'lodash';
 
-export default () => {
-  program.version('0.0.1');
-}; */
+// export const printCWD = () => console.log(`Current cwd: ${process.cwd()}`);
+
+export default (filepath1, filepath2) => {
+  // Resolve the both paths and connect with the absolute path with process.cwd
+  const absPath = toString(process.cwd);
+  const path1 = path.resolve(absPath, filepath1);
+  const path2 = path.resolve(absPath, filepath2);
+  // console.log('Path1: ', path1, 'Path2: ', path2);
+
+  // Read both files
+  const file1 = fs.readFileSync(path1);
+  const file2 = fs.readFileSync(path2);
+
+  // Parse both files to -> objects
+  const data1 = JSON.parse(file1);
+  const data2 = JSON.parse(file2);
+
+  // console.log for debugging purposes =)
+  /*
+  console.log('--------');
+  console.log('Object 1');
+  console.log('--------');
+  Object.entries(data1).map(([key, value]) => console.log(`${key} : ${value}`));
+  console.log('--------');
+  console.log('Object 2');
+  console.log('--------');
+  Object.entries(data2).map(([key, value]) => console.log(`${key} : ${value}`));
+  console.log('--------');
+  */
+
+  // Form an array of unique keys from both files
+  const united = _.union(Object.keys(data1), Object.keys(data2));
+  const result = united.reduce((acc, key) => {
+    // if a key of file1 is not present in file2
+    if (!data2[key]) {
+      return [...acc, ` - ${key} : ${data1[key]}`];
+    }
+    // if a key of file2 is not present in file1
+    if (!data1[key]) {
+      return [...acc, ` + ${key} : ${data2[key]}`];
+    }
+    // if a key of file1 is present but the value is different
+    if (data1[key] !== data2[key]) {
+      return [...acc, ` - ${key} : ${data1[key]}`, ` + ${key} : ${data2[key]}`];
+    }
+    // if a key and value is the same for both files
+    return [...acc, `   ${key} : ${data1[key]}`];
+  }, []);
+  // const result = Object
+  return ['{', ...result, '}'].join('\n');
+};

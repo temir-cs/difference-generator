49b3c9c2e487f9063ab43d9c35522a22ca8ef69a
temir-cs <temirlan.akhanov@yandex.ru>
2020-09-15 23:32:32 +0600

    modified plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,40 +1,43 @@
+import _ from 'lodash';
 // Plain text formatter
 
 // format string based on the type of value
 const formatValue = (value) => {
-  const values = {
-    object: '[complex value]',
-    string: `'${value}'`,
-    boolean: value,
-  };
-  return values[typeof value];
+  switch (typeof value) {
+    case 'object':
+      return '[complex value]';
+    case 'string':
+      return `'${value}'`;
+    // other value types (e.g.: numbers, etc) are not formatted and returned as is
+    default:
+      return value;
+  }
 };
 
-// build a full sentence based on the item status
-const makeSentence = (beginning, item) => {
-  const endings = {
-    updated: `updated. From ${formatValue(item.before)} to ${formatValue(item.after)}`,
-    added: `added with value: ${formatValue(item.value)}`,
-    removed: 'removed',
-  };
-  const result = `${beginning}${endings[item.status]}`;
-  return result;
+// select a proper method based on an entry status
+const mapping = {
+  // if a current item is a tree - traverse it recursively
+  nested: (item, head, path, iter) => iter(item.children, path),
+  // add a proper ending
+  updated: (item, head) => `${head}updated. From ${formatValue(item.before)} to ${formatValue(item.after)}`,
+  added: (item, head) => `${head}added with value: ${formatValue(item.value)}`,
+  removed: (item, head) => `${head}removed`,
+  // if status is unchanged - return an empty array that will be flattened by flatMap()
+  unchanged: () => [],
 };
 
 const plain = (diff) => {
   const iter = (entries, path) => {
-    // first filter items that are unchanged - we don't need them
-    const result = entries.filter((entry) => entry.status !== 'unchanged')
-      .map((entry) => {
-        // add name of a current element to path and build a path string
-        const currentPath = [...path, entry.name];
-        const pathStr = currentPath.join('.');
-        const head = `Property '${pathStr}' was `;
-        // if a current item is a tree - traverse it recursively
-        // otherwise - build a sentence
-        const sentence = entry.status === 'nested' ? iter(entry.children, currentPath) : makeSentence(head, entry);
-        return sentence;
-      });
+    // flatMap needed to handle values with 'unchanged' status
+    const result = _.flatMap(entries, (entry) => {
+      // add name of a current element to path and build a path string
+      const currentPath = [...path, entry.name];
+      const pathStr = currentPath.join('.');
+      const head = `Property '${pathStr}' was `;
+      const sentence = mapping[entry.status](entry, head, currentPath, iter);
+      // console.log('Sentence: ', sentence);
+      return sentence;
+    });
     return result.join('\n');
   };
   return iter(diff, []);
